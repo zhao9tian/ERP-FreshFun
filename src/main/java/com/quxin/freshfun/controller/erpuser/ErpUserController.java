8677c8b768f079cb5507485c6e30a0a3df966d63
Ziming <1065214457@qq.com>
2016-11-02 14:51:44 +0800

    后台用户体系

--- src/main/java/com/quxin/freshfun/controller/erpuser/ErpUserController.java
+++ src/main/java/com/quxin/freshfun/controller/erpuser/ErpUserController.java
@@ -8,6 +8,7 @@ import com.quxin.freshfun.utils.ResultUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -24,6 +25,7 @@ import static javax.management.Query.value;
  * 后台用户controller
  */
 @RequestMapping("/erpuser")
+@Controller
 public class ErpUserController {
     @Autowired
     private ErpUserService erpUserService;
