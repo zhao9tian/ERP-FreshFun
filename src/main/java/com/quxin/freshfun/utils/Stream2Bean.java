6896962a7606cd6df5d4a44272df4ad5c919ba9c
渠成 <2830224613@qq.com>
2016-12-06 19:00:58 +0800

    空数据校验

--- src/main/java/com/quxin/freshfun/utils/Stream2Bean.java
+++ src/main/java/com/quxin/freshfun/utils/Stream2Bean.java
@@ -27,37 +27,38 @@ public class Stream2Bean {
 
     /**
      * 转换excel流为订单list --只包含物流信息
+     *
      * @param inputStream 文件流
      * @return 订单列表
      */
     public static List<OrderDetailsPOJO> getOrdersByInputStream(InputStream inputStream) {
         try {
             List<OrderDetailsPOJO> orderDetailsPOJOs = new ArrayList<>();
-            FileInputStream fis = (FileInputStream) inputStream;//文件流
+            FileInputStream fis = (FileInputStream) inputStream;
             Workbook workbook = new XSSFWorkbook(fis);//整个excel对象
-            Sheet sheet = workbook.getSheetAt(0);//此处只有一个
-            for (int row = 1; row <= sheet.getLastRowNum(); row++) {//去除第一行
+            Sheet sheet = workbook.getSheetAt(0);//此处只取第一个sheet
+            for (int row = 1; row <= sheet.getLastRowNum(); row++) {//不取title
                 Row rowObject = sheet.getRow(row);
-                if(rowObject.getCell(0).getCellType()!=Cell.CELL_TYPE_BLANK){//没有数据了
+                if (rowObject.getCell(0) != null) {//没有数据了
                     OrderDetailsPOJO orderDetailsPOJO = new OrderDetailsPOJO();
                     try {
-                        if(rowObject.getCell(0).getCellType()!=Cell.CELL_TYPE_BLANK){
-                            rowObject.getCell(0).setCellType(Cell.CELL_TYPE_STRING);
-                            Long orderId = Long.parseLong(rowObject.getCell(0).getStringCellValue());//订单号
-                            orderDetailsPOJO.setOrderId(orderId);
+                        rowObject.getCell(0).setCellType(Cell.CELL_TYPE_STRING);//将数字变为字符
+                        Long orderId = Long.parseLong(rowObject.getCell(0).getStringCellValue());//订单号
+                        orderDetailsPOJO.setOrderId(orderId);
+                        if (rowObject.getCell(1)!= null) {
+                            String shipperCode = rowObject.getCell(1).getStringCellValue();//公司编号
+                            orderDetailsPOJO.setDeliveryName(shipperCode);
                         }
-                        if(rowObject.getCell(2).getCellType()!=Cell.CELL_TYPE_BLANK){
+                        if (rowObject.getCell(2) != null) {
                             rowObject.getCell(2).setCellType(Cell.CELL_TYPE_STRING);
                             String logisticCode = rowObject.getCell(2).getStringCellValue(); //物流单号
                             orderDetailsPOJO.setDeliveryNum(logisticCode);
                         }
-                        if(rowObject.getCell(3).getCellType()!=Cell.CELL_TYPE_BLANK){
+                        if (rowObject.getCell(3) != null) {
                             rowObject.getCell(3).setCellType(Cell.CELL_TYPE_STRING);
                             String goodscost = rowObject.getCell(3).getStringCellValue(); //成本价
                             orderDetailsPOJO.setActualMoney(goodscost);
                         }
-                        String shipperCode = rowObject.getCell(1).getStringCellValue();//公司编号
-                        orderDetailsPOJO.setDeliveryName(shipperCode);
                         orderDetailsPOJOs.add(orderDetailsPOJO);
                     } catch (ClassCastException e) {
                         logger.error("订单号为" + rowObject.getCell(0).getStringCellValue() + "的记录格式错误");
@@ -65,7 +66,7 @@ public class Stream2Bean {
                 }
             }
             fis.close();
-            return orderDetailsPOJOs ;
+            return orderDetailsPOJOs;
         } catch (IOException e) {
             logger.error("关闭文件流异常");
         }
